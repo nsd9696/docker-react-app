3abb4ab8e3f0e388cd3d282fa09a873521cced8e
nsd9696 <nsd26534613@gmail.com>
2021-02-28 17:46:06 +0900

    travis commit5

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn More 
+          Learn React 
         </a>
       </header>
     </div>
